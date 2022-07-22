2cce659234e6e5cf48487844107befc7f63caf1d
Maksym Tymchenko <maximgst@gmail.com>
2022-07-22 21:33:50 +0300

    com

---
+++ project/js/scroll.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.header__list');
+const anchors = nav.querySelectorAll('a[href*="#"]')
+console.log(anchors);
+for (let anchor of anchors) {
+   anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      const blockID = anchor.getAttribute('href').substr(1)
+
+      document.getElementById(blockID).scrollIntoView({
+         behavior: 'smooth',
+         block: 'start'
+      })
+   })
+}
